5ddf3ff8e9b89aac9e244a0f8e0ffdd82280c777
Ivoo86 <Ivoo.Francis@gmail.com>
2026-03-17 23:21:18 +0200

    Add country lists and integrate them in client generator

--- generate_clients.py
+++ generate_clients.py
@@ -7,6 +7,22 @@ from faker import Faker
 #Initialize faker generator
 fake = Faker()
 
+#Load countries from txt file
+def load_country_list(file_path):
+    with open(file_path, "r",
+encoding="utf-8") as f:
+        return [line.strip() for line
+in f if line.strip()]
+
+#Load AML country risk lists  
+fatf_black = load_country_list("SQL/fatf_black_list.txt")
+eu_high = load_country_list("SQL/eu_high_risk_countries.txt")
+grey_list = load_country_list("SQL/fatf_grey_list.txt")
+
+#Define risk groups
+high_risk_countries = list(set(fatf_black + eu_high))
+medium_risk_countries = grey_list
+
 #SQL server connection
 conn = pyodbc.connect(
      "DRIVER={ODBC Driver 17 for SQL Server};"
@@ -14,7 +30,7 @@ conn = pyodbc.connect(
     "DATABASE=TransactionMonitoringProject;"
     "Trusted_Connection=yes;"
 )
-
+#Ceate cursor to execute SQL queries
 cursor = conn.cursor()
 
 #Load scenario configuration from JSON file
@@ -42,35 +58,6 @@ risk_levels = (
 #Shuffle risk clients order so clients are randomized
 random.shuffle(risk_levels)
 
-#Load high risk countries from database risk rules
-cursor.execute("""
-    SELECT DISTINCT RuleValue
-    FROM dbo.RiskRules_table
-    WHERE RuleType = 'Country'
-         AND RiskLevel = 'High'
-         AND IsActive = 1
- """)
-
-#Convert SQL result to Python list
-high_risk_countries = [row[0].strip()
-for row in cursor.fetchall() if row[0]]
-
-#Load medium risk countries from database risk rules
-cursor.execute("""
-    SELECT DISTINCT RuleValue
-    FROM dbo.RiskRules_table
-    WHERE RuleType = 'Country'
-         AND RiskLevel = 'Medium'
-         AND IsActive = 1
- """)
-
-#Convert SQL result to python list
-medium_risk_countries = [row[0].strip()
-for row in cursor.fetchall() if row[0]]
-
-print("High risk countries:", high_risk_countries)
-print("Medium risk countries:", medium_risk_countries)
-
 #Simulate account status distribution, more active accounts
 statuses = ["Active", "Active", "Active", "Blocked"]
 
@@ -80,13 +67,18 @@ clients = []
 #Clear all generated cients before insert
 cursor.execute("DELETE FROM dbo.Clients_table")
 
+high_risk_countries = list(set(fatf_black + eu_high))
+medium_risk_countries = grey_list
+
+all_countries = [fake.country() for _ in range(500)]
+low_risk_countries = list(set(all_countries) - set(high_risk_countries) - set(medium_risk_countries))
 #Generte client records based on scenario configuration
 for i in range(total_clients):
 #Assign country based on risk level
     risk = risk_levels[i] 
     if risk == 3: country = random.choice(high_risk_countries)        
     elif risk == 2: country = random.choice(medium_risk_countries)        
-    else: country = fake.country()         
+    else: country = random.choice(low_risk_countries)         
 
 #Create synthetic client record, data simulates realistic KYC onboarding information
     client = {
@@ -102,14 +94,14 @@ for i in range(total_clients):
     clients.append(client)
 
 #Insert generated clients into SQL server
-cursor.execute("""
+    cursor.execute("""
     INSERT INTO dbo.Clients_table
     (Name, Country, Address, Onboardingdate, DateOfBirth, KYCRiskLevelID, Status)
     VALUES (?, ?, ?, ?, ?, ?, ?)
 """,
     client["Name"],
     client["Country"],
-    client["Adress"],    
+    client["Address"],    
     client["OnboardingDate"],
     client["DateOfBirth"],
     client["KYCRiskLevelID"],
